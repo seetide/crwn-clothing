d0f6bff64e248fa8fc883ea2ce978cc66d9a607c
seetide <seetide@gmail.com>
2019-09-10 22:11:09 -0400

    change customButton style with styled

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 
-import './custom-button.style.scss';
+import { CustomButtonContainer } from './custom-button.styles';
+//import './custom-button.style.scss';
 
-const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => (
-  <button
-    className={`${inverted ? 'inverted' : ''}
-    ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
-    {...otherProps}
-  >
-    {children}
-  </button>
+const CustomButton = ({ children, ...props }) => (
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
